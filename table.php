9395ce3ac188933f3e56585d632b6a2966b0e12c
EMNGz <gameball326@gmail.com>
2024-01-29 16:30:50 +0700

    table

---
+++ table.php
@@ -0,0 +1,113 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.3.2/css/all.min.css" integrity="sha384-xZQk8J6Hbe7WNfA5d+J+26W+5j8wz5+U2TTCKbO+5aF8gBsf5zI+IkA6IHvmKp2" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <link rel="stylesheet" type="text/css" href="table.css">
+</head>
+<body>
+
+
+
+
+    <div class="row">
+      <div >
+     
+  </div>
+      <table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col" colspan="4" class="headcontainer "> อัตรากำลัง   </th>
+    
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"  colspan="4"  class="secheadtable ">ข้อมูลอัตรากำลัง กองกำกับการสืบสวนปรามปราม กองบังคับการตรวจคนเข้าเมือง 2
+        </th>
+    </tr>
+    <tr>
+      <th scope="row" colspan="4"  class="botheadtable ">อัตรากำลังชั้นสัญญาบัตร กองกำกับการสืบสวนปราบปราม (ข้อมูล ณ 1 มกราคม 2567)
+        </th>    
+    </tr>
+    <tr class="lightorange1">
+    <th scope="row">ตำแหน่ง ชั้นยศ</th>
+      <th>อัตราการอนุญาต</th>
+      <th>คนครอง</th>
+      <th>ปฎิบัติงานจริง</th>
+    </tr>
+    <tr class="lightgreen1">
+    <td scope="row" class="lightorange1">ผกก.</td>
+      <td>1</td>
+      <td>1</td>
+      <td>1</td>
+    </tr>
+    <tr class="darkgreen1">
+    <td scope="row" class="lightorange1">รอง ผกก.</td>
+      <td>2</td>
+      <td>2</td>
+      <td>2</td>
+    </tr>
+    <tr class="lightgreen1">
+    <td scope="row" class="lightorange1">สว.</td>
+      <td>5</td>
+      <td>5</td>
+      <td>5</td>
+    </tr>
+    <tr class="darkgreen1">
+    <td scope="row" class="lightorange1">รอง สว.</td>
+      <td>21</td>
+      <td>13</td>
+      <td>13</td>
+    </tr>
+    <tr class="lightgreen1">
+    <td scope="row" class="lightorange1">รอง สว. (กดต.53)</td>
+      <td>14</td>
+      <td>14</td>
+      <td>14</td>
+    </tr>
+    <tr class="darkorange1">
+    <th scope="row" class="lightorange1">รวม</th>
+      <th>43</th>
+      <th>35</th>
+      <th>35</th>
+    </tr>
+    <tr>
+      <th scope="row" colspan="4"  class="botheadtable ">อัตรากำลังชั้นสัญญาบัตร กองกำกับการสืบสวนปราบปราม (ข้อมูล ณ 1 มกราคม 2567) </th>     
+    </tr>
+    <tr class="lightorange1">
+    <th scope="row">ตำแหน่ง ชั้นยศ</th>
+      <th>อัตราการอนุญาต</th>
+      <th>คนครอง</th>
+      <th>ปฎิบัติงานจริง</th>
+    </tr>   
+
+    <tr class="lightgreen1">
+    <td scope="row" class="lightorange1">ผบ.หมู่
+</td>
+      <td>68</td>
+      <td>56</td>
+      <td>56</td>
+    </tr>
+
+
+    <tr class="darkorange1">
+    <th scope="row" class="lightorange1">รวม</th>
+      <th>111</th>
+      <th>91</th>
+      <th>91</th>
+    </tr>
+    <tr>
+  </tbody>
+</table>
+    </div>
+        </div>
+            </div>
+</body>
+
+
